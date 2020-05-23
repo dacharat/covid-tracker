ef1f1bfcdc310da6e2129c3ca9248cd09cea94aa
dacharat <dacharat.p@ku.th>
2020-05-23 21:53:48 +0700

    :octocat: add see more

--- src/components/home/CountryCase.tsx
+++ src/components/home/CountryCase.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import Link from 'next/link'
 import styled from 'styled-components'
 
 import { HomeContext } from '@utils/context'
@@ -12,6 +13,23 @@ const Container = styled(C)`
   width: 100%;
   max-width: 1250px;
 `
+const SeeMoreView = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #d6d6d6;
+`
+const SeeMore = styled.a`
+  font-size: 20px;
+  padding: 10px;
+  color: #000;
+  line-height: 28px;
+  transition: font-size 0.5s ease;
+  :hover {
+    font-size: 22px;
+    color: #000;
+  }
+`
 
 const CountryCase = () => {
   const { selectedCountry } = useContext(HomeContext)
@@ -20,6 +38,13 @@ const CountryCase = () => {
     <Container>
       <CountrySelector />
       <Case caseData={selectedCountry} />
+      {selectedCountry && (
+        <SeeMoreView>
+          <Link href={`/countries/${selectedCountry.slug}`}>
+            <SeeMore>See More</SeeMore>
+          </Link>
+        </SeeMoreView>
+      )}
     </Container>
   )
 }
--- src/pages/countries/[country].tsx
+++ src/pages/countries/[country].tsx
@@ -2,12 +2,12 @@ import { GetServerSideProps } from 'next'
 import { internalAPI } from '@utils/constant'
 
 const Country = ({ data }) => {
-  console.log(data)
-
-  return <div>Hello</div>
+  return <div>{JSON.stringify(data, null, 2)}</div>
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
+  console.log(req)
+
   if (!req) {
     return { props: {} }
   }
